54a671a28c2f69e0a794b600ed0dfb5718277347
Tymon Tian <tymon.tian@student.tdsb.on.ca>
2023-01-12 15:21:31 -0500

    Final Version

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "../views/Login.vue";
+import Home from "../views/homeview.vue";
+import Movie from "../views/movie.vue";
+import Cart from "../views/cart.vue";
+
+const routes = [
+  {
+    path: '/',
+    component:Home,
+  },
+  {
+    path: '/Login',
+    component:Login,
+  },
+  {
+    path: '/Movie',
+    component: Movie,
+  },
+  {
+    path: '/Cart',
+    component: Cart,
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
